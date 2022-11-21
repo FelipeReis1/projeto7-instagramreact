3c337d5d14fa1a6a0b7427744e548ad9a56cf4b6
FelipeReis1 <feliperg15@yahoo.com.br>
2022-11-21 15:11:20 -0300

    tratada string vazia - instagram react finalizado

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -5,10 +5,20 @@ function Usuario(props) {
   const [foto, setFoto] = React.useState("assets/img/catanacomics.svg");
 
   function alterarNome() {
-    setNome(prompt("Digite seu nome"));
+    let nome = prompt("Digite seu nome");
+    if (nome !== "") {
+      setNome(nome);
+    } else {
+      prompt("Formato de nome inválido! Digite seu nome novamente");
+    }
   }
   function alterarFoto() {
-    setFoto(prompt("Insira sua foto"));
+    let foto = prompt("Insira um link de imagem para sua foto");
+    if (foto !== "") {
+      setFoto(foto);
+    } else {
+      prompt("Formato de foto não reconhecido! Insira um link válido");
+    }
   }
 
   return (
